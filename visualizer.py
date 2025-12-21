cb90a2c1cbe8271ab81dd30b39fe67149ceb57bb
Sushant458 <yadniksushant@gmail.com>
2025-12-21 13:12:29 +0530

    Add files via upload

---
+++ visualizer.py
@@ -0,0 +1,38 @@
+import random
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPainter, QColor, QPen
+
+class WaveVisualizer(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.amplitude = [random.randint(30, 80) for _ in range(100)]  # Initial wave heights
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_wave)
+        self.timer.start(100)  # Update every 100ms
+
+    def update_wave(self):
+        """Randomly change wave heights for animation effect"""
+        self.amplitude = [random.randint(30, 80) for _ in range(100)]
+        self.update()  # Redraw the widget
+
+    def paintEvent(self, event):
+        """Draw the animated sound waves"""
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        width = self.width()
+        height = self.height()
+        num_lines = len(self.amplitude)
+        spacing = width // (num_lines + 1)
+
+        # Draw wave bars
+        for i in range(num_lines):
+            x = (i + 1) * spacing
+            y = height // 2
+            bar_height = self.amplitude[i]
+
+            pen = QPen(QColor(0, 255, 0))  # Green color for waves
+            pen.setWidth(6)  # Thickness of wave lines
+            painter.setPen(pen)
+            painter.drawLine(x, y - bar_height, x, y + bar_height)
